2798208f74fa857e94bf11f7a7756f69ad2b842c
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-22 19:39:21 +0300

    Create pset2-10.cpp

---
+++ pset2-10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+  int first_num = 0;
+  int second_num = 0;
+  cin >> first_num;
+  cin >> second_num;
+
+  // Number One => 10
+  // Number Two => 100
+  cout << first_num * second_num << "\n";
+  // Needed Output 1000 [10 * 100]
+  return 0;
+}
