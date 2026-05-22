0c9f5b0d0d531ee426eb352dc106f39fb723a05b
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-22 19:35:51 +0300

    Create pset2-9.cpp

---
+++ pset2-9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+  int age = 0;
+  cin >> age;
+
+  // Num 10
+  cout << age * age << "\n";
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
