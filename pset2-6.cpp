bc003850aa4cfaf3add2d06f4f383abdfd76f5a9
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-22 19:25:39 +0300

    Create pset2-6.cpp

---
+++ pset2-6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Do Not Edit Below
+  int a = 210, b = 50, c = 40;
+
+  // Write Your cout Code Here
+  cout << a + b + c << "\n";
+
+  return 0;
+}
+// NOTE: In Elzero Academy, this challenge is -50 and -40, but I fixed it because it is a logical error.
