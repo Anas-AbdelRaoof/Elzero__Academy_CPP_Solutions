6b672cfcb61aa9bded068f6c169068ce9261725f
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-22 19:32:59 +0300

    Create pset2-8.cpp

---
+++ pset2-8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+  // Edit Here To Fix The Error
+  int current_year; // Remove "const".
+  int birth_year; // Remove "const".
+
+  // Do Not Edit Here
+  current_year = 2022;
+  birth_year = 1982;
+  int age_in_years = current_year - birth_year;
+  std::cout << age_in_years << "\n";
+  return 0;
+}
