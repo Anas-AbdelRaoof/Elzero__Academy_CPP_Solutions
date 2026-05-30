d110bf82a3f1189331cd5d327b71b658b825c021
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-30 19:44:47 +0300

    Create pset3-7.cpp

---
+++ pset3-7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 1;
+    int b = 13;
+    int c = 17;
+    int d = 70;
+
+    // Output Needed
+    cout << char(d - a) << char(c + d) << char(d + b) << "\n"; // "EWS"
+}
