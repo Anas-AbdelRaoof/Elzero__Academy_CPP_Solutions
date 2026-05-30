a240066b4aa9dc328d1c0ee114a8cd78bd94af03
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-30 19:43:33 +0300

    Create pset3-5.cpp

---
+++ pset3-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a = '~';
+    char b = '&';
+    char c = '%';
+    char d = 'A';
+
+    // Output Needed
+    cout << "ASCII Value of ~ Is " << int(a) << "\n";
+    cout << "ASCII Value of & Is " << int(b) << "\n";
+    cout << "ASCII Value of % Is " << int(c) << "\n";
+    cout << "ASCII Value of A Is " << int(d) << "\n";
+}
+
