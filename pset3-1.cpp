3bd3137d0e3c5badaf884984bb9270e258ccfc1d
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-30 19:41:00 +0300

    Create pset3-1.cpp

---
+++ pset3-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // pset 1
+    // Example
+    false; // bool => Boolean
+
+    'a'; // char
+    '9'; // char
+    9; // int
+    true; // bool
+    100; // int
+    -10; // int
+    0; // int
+    false; // bool
+    10.9; // float or double
+}
