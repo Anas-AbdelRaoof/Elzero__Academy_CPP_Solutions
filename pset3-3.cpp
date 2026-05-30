7c2e05ac2ff22e317fcc40020a204bbef23e7d58
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-05-30 19:42:23 +0300

    Create pset3-3.cpp

---
+++ pset3-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+{
+    // Output Needed
+    cout << "Maximum Integer Number Is => " << INT_MAX << "\n";
+    cout << "Minimum Integer Number Is => " << INT_MIN << "\n";
+    cout << "Maximum Short Integer Number Is => " << SHRT_MAX << "\n";
+    cout << "Minimum Short Integer Number Is => " << SHRT_MIN << "\n";
+}
